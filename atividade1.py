43de651fd967e629c0fbaf50666cc476a18e6fe4
diogosantoro <81193631+diogosantoro@users.noreply.github.com>
2021-03-31 15:54:13 -0300

    Add files via upload

---
+++ atividade1.py
@@ -0,0 +1,49 @@
+
+soma = 0
+lista = [1, 1]
+
+def calcula_soma(i):
+    i = numeros
+for soma in lista:
+    soma += int(soma)
+
+print(soma)
+
+
+
+
+i = "4 5 6 7 8"
+numeros = i.split()
+lista = []
+def converte_entrada(numeros):
+    for numero in numeros:
+
+        lista.append(int(numero))
+    return lista
+lista = converte_entrada(numeros)
+print(lista)
+
+
+i = [1, 4, 7, 9]
+val1 = 0
+val2 = len(i)
+
+def processa_numeros(numeros):
+    i = numeros
+for numeros in i:
+    val1 += int(numeros)
+print(val1)
+print(val2)
+
+
+
+
+
+
+
+
+
+
+
+
+
